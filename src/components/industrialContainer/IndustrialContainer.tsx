894b3fee6488fbe15949e66b17d9e3bc9ae5a75e
johannpereze <sebas.jsp@gmail.com>
2022-08-27 12:51:16 -0500

    fix: renamed industrialBox to industrialContainer

--- src/components/industrialContainer/IndustrialContainer.tsx
+++ src/components/industrialContainer/IndustrialContainer.tsx
@@ -1,18 +1,21 @@
 import { Box, SxProps, Theme } from "@mui/material";
 import { ReactNode } from "react";
+import imgUrl from "../../static/images/container-border.png";
 
-interface IndustrialPaperProps {
+interface IndustrialContainerProps {
   children: ReactNode;
   sx?: SxProps<Theme> | undefined;
 }
 
-export default function IndustrialBox({ children, sx }: IndustrialPaperProps) {
+export default function IndustrialContainer({
+  children,
+  sx,
+}: IndustrialContainerProps) {
   return (
     <Box
       sx={{
         border: "10px solid",
-        borderImage:
-          "url(https://firebasestorage.googleapis.com/v0/b/medabotsapp.appspot.com/o/container-border.png?alt=media&token=48e72d0d-ff26-477f-87fb-fafc0c08020e) 10",
+        borderImage: `url(${imgUrl}) 10`,
         borderImageSlice: "10",
         borderImageOutset: "1",
         imageRendering: "pixelated",
